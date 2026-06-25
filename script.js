93c8c23d88f857300070c777cabafbaffc6d5fbe
marilgg <mari.l0530g@gmail.com>
2026-06-25 05:56:44 -0500

    Crear primera version funcional de mindly

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Mindly iniciada");
+let completedTasks = 0;
+
+function addTask(){
+
+    const input = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+
+    if(input.value.trim() === ""){
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = input.value;
+
+    li.addEventListener("click", function(){
+
+        if(!li.classList.contains("completed")){
+            li.classList.add("completed");
+
+            completedTasks++;
+
+            document.getElementById("completed").textContent = completedTasks;
+        }
+
+    });
+
+    taskList.appendChild(li);
+
+    input.value = "";
+}
