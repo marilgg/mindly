1701a73effce16159b4ef0d4d190394bd335c4ec
marilgg <mari.l0530g@gmail.com>
2026-07-18 09:02:00 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,5 @@
+const boton = document.getElementById("boton");
 
+boton.addEventListener("click", function () {
+    alert("¡Bienvenido a Mindly! Muy pronto podrás organizar todas tus tareas.");
+});
